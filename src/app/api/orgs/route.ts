c7de3c9bd32d14c982c5eebc621b82066ffb408a
Saklfash <saklfash@gmail.com>
2025-12-30 23:29:37 +0100

    chore/configure eslint

--- src/app/api/orgs/route.ts
+++ src/app/api/orgs/route.ts
@@ -1,24 +1,28 @@
 import { NextResponse } from 'next/server';
 type Org = {
-    orgName: string
-    needs: {
-        name: string
-        link: string
-    }[]
-}
+  orgName: string;
+  needs: {
+    name: string;
+    link: string;
+  }[];
+};
 
-const inMemoryDB: Org[] = [{
-    orgName: "Fundacja X",
-    needs: [{
-        name: "karma",
-        link: "https://allegro.pl/123"
-    },
-{
-        name: "koce",
-        link: "https://allegro.pl/321"
-    }]
-}]
+const inMemoryDB: Org[] = [
+  {
+    orgName: 'Fundacja X',
+    needs: [
+      {
+        name: 'karma',
+        link: 'https://allegro.pl/123',
+      },
+      {
+        name: 'koce',
+        link: 'https://allegro.pl/321',
+      },
+    ],
+  },
+];
 
 export async function GET() {
-        return NextResponse.json(inMemoryDB)
+  return NextResponse.json(inMemoryDB);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
+const { NEXT_PUBLIC_BASE_URL = 'http://localhost:3000' } = process.env;
 
 export default async function Home() {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/orgs`)
-  const orgs = await data.json()
+  const data = await fetch(`${NEXT_PUBLIC_BASE_URL}/api/orgs`);
+  const orgs = await data.json();
   return (
     <div>
       <main>
-        {orgs.map(({orgName, needs}) => (
+        {orgs.map(({ orgName, needs }) => (
           <div key={orgName}>
             <h1>{orgName}</h1>
             <h3>Potrzeby:</h3>
             <ul>
-              {needs.map(({name, link}) => (
-                <li key={link}><a href={link}>{name}</a></li>
+              {needs.map(({ name, link }) => (
+                <li key={link}>
+                  <a href={link}>{name}</a>
+                </li>
               ))}
             </ul>
           </div>
         ))}
-        
       </main>
     </div>
   );
