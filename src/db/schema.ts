dc4ca222e668654a9386289c4a3dd90684b5f645
Pawel-Mackiewicz <pawel@mackiewicz.info>
2026-03-07 11:28:35 +0100

    chore: add test database schema and journal config files

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+
+// stub data to test logic
+export const usersTable = pgTable('users', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
