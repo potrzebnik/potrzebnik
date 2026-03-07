98126978f2a789262513f25dd1a334e348485a26
Pawel-Mackiewicz <pawel@mackiewicz.info>
2026-03-07 12:20:57 +0100

    chore: remove test database schema file

--- src/db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
-
-// stub data to test logic
-export const usersTable = pgTable('users', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
