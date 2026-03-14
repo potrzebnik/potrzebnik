c28cfc52ee589ecb563c4f5aa5b4b20564025282
Pawel-Mackiewicz <pawel@mackiewicz.info>
2026-03-14 12:31:17 +0100

    feat: export db instance and add auth module with drizzle adapter

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,4 +2,4 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { resolveDatabaseUrl } from './resolve-database-url';
 
-const db = drizzle(resolveDatabaseUrl());
+export const db = drizzle(resolveDatabaseUrl());
