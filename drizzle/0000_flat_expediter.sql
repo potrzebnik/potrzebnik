1aac760591e81484b5fed93c24cf2a07689e05af
Pawel-Mackiewicz <pawel@mackiewicz.info>
2026-03-15 20:16:42 +0100

    fix: migrations

--- drizzle/0000_flat_expediter.sql
+++ drizzle/0000_flat_expediter.sql
@@ -1,6 +1,5 @@
 CREATE TABLE "addresses" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"organization_id" integer,
 	"city" text NOT NULL,
 	"street" text NOT NULL,
 	"building" text NOT NULL,
@@ -47,6 +46,24 @@ CREATE TABLE "donors" (
 	"updated_at" timestamp with time zone NOT NULL,
 	"deleted_at" timestamp with time zone,
 	CONSTRAINT "donors_user_id_unique" UNIQUE("user_id")
+);
+--> statement-breakpoint
+CREATE TABLE "address_types" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"code" text NOT NULL,
+	"created_at" timestamp with time zone NOT NULL,
+	"updated_at" timestamp with time zone NOT NULL,
+	"deleted_at" timestamp with time zone,
+	CONSTRAINT "address_types_code_unique" UNIQUE("code")
+);
+--> statement-breakpoint
+CREATE TABLE "organization_addresses" (
+	"organization_id" integer NOT NULL,
+	"address_id" integer NOT NULL,
+	"address_type_id" integer NOT NULL,
+	"created_at" timestamp with time zone NOT NULL,
+	"deleted_at" timestamp with time zone,
+	CONSTRAINT "organization_addresses_pkey" PRIMARY KEY("organization_id","address_id","address_type_id")
 );
 --> statement-breakpoint
 CREATE TABLE "organization_categories" (
@@ -145,11 +162,13 @@ CREATE TABLE "donations" (
 	"deleted_at" timestamp with time zone
 );
 --> statement-breakpoint
-ALTER TABLE "addresses" ADD CONSTRAINT "addresses_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "users" ADD CONSTRAINT "users_role_id_user_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."user_roles"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "donor_favourite_needs" ADD CONSTRAINT "donor_favourite_needs_donor_id_donors_id_fk" FOREIGN KEY ("donor_id") REFERENCES "public"."donors"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "donor_favourite_needs" ADD CONSTRAINT "donor_favourite_needs_need_id_needs_id_fk" FOREIGN KEY ("need_id") REFERENCES "public"."needs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "donors" ADD CONSTRAINT "donors_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "organization_addresses" ADD CONSTRAINT "organization_addresses_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "organization_addresses" ADD CONSTRAINT "organization_addresses_address_id_addresses_id_fk" FOREIGN KEY ("address_id") REFERENCES "public"."addresses"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "organization_addresses" ADD CONSTRAINT "organization_addresses_address_type_id_address_types_id_fk" FOREIGN KEY ("address_type_id") REFERENCES "public"."address_types"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "organizations" ADD CONSTRAINT "organizations_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "organizations" ADD CONSTRAINT "organizations_category_id_organization_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."organization_categories"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "need_status_history" ADD CONSTRAINT "need_status_history_need_id_needs_id_fk" FOREIGN KEY ("need_id") REFERENCES "public"."needs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
@@ -163,5 +182,8 @@ ALTER TABLE "needs" ADD CONSTRAINT "needs_type_id_need_types_id_fk" FOREIGN KEY 
 ALTER TABLE "donations" ADD CONSTRAINT "donations_donor_id_donors_id_fk" FOREIGN KEY ("donor_id") REFERENCES "public"."donors"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "donations" ADD CONSTRAINT "donations_need_id_needs_id_fk" FOREIGN KEY ("need_id") REFERENCES "public"."needs"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "donations" ADD CONSTRAINT "donations_status_id_donation_statuses_id_fk" FOREIGN KEY ("status_id") REFERENCES "public"."donation_statuses"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "addresses_organization_id_idx" ON "addresses" USING btree ("organization_id");--> statement-breakpoint
-CREATE INDEX "needs_organization_id_idx" ON "needs" USING btree ("organization_id");
+CREATE INDEX "need_status_history_need_id_idx" ON "need_status_history" USING btree ("need_id");--> statement-breakpoint
+CREATE INDEX "need_status_history_changed_by_id_idx" ON "need_status_history" USING btree ("changed_by_id");--> statement-breakpoint
+CREATE INDEX "needs_organization_id_idx" ON "needs" USING btree ("organization_id");--> statement-breakpoint
+CREATE INDEX "donations_donor_id_idx" ON "donations" USING btree ("donor_id");--> statement-breakpoint
+CREATE INDEX "donations_need_id_idx" ON "donations" USING btree ("need_id");
