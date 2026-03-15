c4e32bd3eba25c34e4987f940204ebc8cf17fa37
Pawel-Mackiewicz <pawel@mackiewicz.info>
2026-03-15 10:24:30 +0100

    fix: migration files

--- drizzle/0001_seed_default_statuses.sql
+++ drizzle/0001_seed_default_statuses.sql
@@ -1,25 +1,25 @@
 INSERT INTO "user_roles" ("code", "created_at", "updated_at")
 VALUES
     ('USER', now(), now()),
-	('DONOR', now(), now()),
-	('ORGANIZATION_ADMIN', now(), now())
+    ('DONOR', now(), now()),
+    ('ORGANIZATION_ADMIN', now(), now())
 ON CONFLICT ("code") DO NOTHING;
 --> statement-breakpoint
 INSERT INTO "need_types" ("code", "created_at", "updated_at")
 VALUES
-	('ONE_TIME', now(), now())
+    ('ONE_TIME', now(), now())
 ON CONFLICT ("code") DO NOTHING;
 --> statement-breakpoint
 INSERT INTO "need_statuses" ("code", "created_at", "updated_at")
 VALUES
-	('ACTIVE', now(), now()),
-	('FULFILLED', now(), now()),
-	('EXPIRED', now(), now())
+    ('ACTIVE', now(), now()),
+    ('FULFILLED', now(), now()),
+    ('EXPIRED', now(), now())
 ON CONFLICT ("code") DO NOTHING;
 --> statement-breakpoint
 INSERT INTO "donation_statuses" ("code", "created_at", "updated_at")
 VALUES
-	('BOOKED', now(), now()),
-	('FULFILLED', now(), now()),
-	('CANCELLED', now(), now())
+    ('BOOKED', now(), now()),
+    ('FULFILLED', now(), now()),
+    ('CANCELLED', now(), now())
 ON CONFLICT ("code") DO NOTHING;
