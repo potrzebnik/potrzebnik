3e50799206449b358e08b6aff3b539f6dd6fcb5f
Pawel-Mackiewicz <pawel@mackiewicz.info>
2026-07-25 21:07:08 +0200

    refactor: auth-config.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,18 @@
-import { db } from '../db';
+import { db } from '@/db';
 import { createAuth } from './auth-config';
+import { getAuthEnv } from './auth-env';
+import { createResendEmailSender } from './emails';
 
 export { createAuth } from './auth-config';
 
+const authEnv = getAuthEnv();
+
 export const auth = createAuth({
   database: db,
+  secret: authEnv.secret,
+  baseURL: authEnv.baseURL,
+  google: authEnv.google,
+  emailSender: authEnv.email
+    ? createResendEmailSender(authEnv.email)
+    : undefined,
 });
