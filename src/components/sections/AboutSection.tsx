9e0924a0ef3e0b7ef0e6d7e460239b0d3d37e4f4
Ania <152223969+LoIlita@users.noreply.github.com>
2026-09-23 18:31:17 +0200

    refactor(ui): rename SectionTemplate to MediaTextSection

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -1,8 +1,8 @@
-import SectionTemplate from '@/components/shared/SectionTemplate';
+import MediaTextSection from '@/components/shared/MediaTextSection';
 
 export default function AboutSection() {
   return (
-    <SectionTemplate
+    <MediaTextSection
       title="O nas"
       descriptions={[
         {
